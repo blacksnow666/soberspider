3f95524617848e90e12ac1c93f1c676b05016284
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2012-10-06 04:31:35 +0800

    exports the schema

--- src/main/java/com/twistlet/soberspider/model/exporter/InformixSchemaExporter.java
+++ src/main/java/com/twistlet/soberspider/model/exporter/InformixSchemaExporter.java
@@ -1,13 +1,101 @@
 package com.twistlet.soberspider.model.exporter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.twistlet.soberspider.model.type.DatabaseColumn;
 import com.twistlet.soberspider.model.type.DatabaseTable;
 
+@Component
 public class InformixSchemaExporter implements SchemaExporter {
 
 	@Override
 	public String exportTable(final DatabaseTable databaseTable) {
-		// TODO Auto-generated method stub
-		return null;
+		final String createStatement = basicCreate(databaseTable);
+		// TODO foreign keys
+		// TODO unique index
+		// TODO non-unique index
+		return createStatement;
+	}
+
+	private String basicCreate(final DatabaseTable databaseTable) {
+		final List<DatabaseColumn> columns = getSortedColumns(databaseTable);
+		final List<String> lines = new ArrayList<>();
+		final StringBuilder sb = new StringBuilder("CREATE TABLE " + databaseTable.getName() + "\n");
+		sb.append("(\n");
+		for (final DatabaseColumn databaseColumn : columns) {
+			final List<String> words = new ArrayList<>();
+			words.add(databaseColumn.getColumnName());
+			words.add(columnType(databaseColumn));
+			if (!databaseColumn.isNullable()) {
+				words.add("NOT NULL");
+			}
+			final String line = "\t" + StringUtils.join(words.toArray(new String[] {}), " ");
+			lines.add(line);
+		}
+		if (databaseTable.getPrimaryKeyColumns().size() == 0) {
+			throw new RuntimeException("no pk for table " + databaseTable.getName());
+		}
+		lines.add("\t" + primaryKeyLine(databaseTable));
+		final String insideCreate = StringUtils.join(lines.toArray(new String[] {}), ",\n");
+		sb.append(insideCreate);
+		sb.append("\n);");
+		return sb.toString();
+	}
+
+	private String primaryKeyLine(final DatabaseTable databaseTable) {
+		final List<String> list = databaseTable.getPrimaryKeyColumns();
+		final String inside = StringUtils.join(list.toArray(new String[] {}), ", ");
+		return "PRIMARY KEY (" + inside + ")";
+	}
+
+	protected String columnType(final DatabaseColumn databaseColumn) {
+		if (databaseColumn.isAutoIncrement()) {
+			return "SERIAL";
+		}
+		switch (databaseColumn.getColumnType()) {
+		case COLUMN_TYPE_VARCHAR:
+			if (databaseColumn.getColumnSize() <= 250) {
+				return "VARCHAR(" + databaseColumn.getColumnSize() + ")";
+			} else {
+				return "LVARCHAR(" + databaseColumn.getColumnSize() + ")";
+			}
+		case COLUMN_TYPE_CHAR:
+			return "CHAR(" + databaseColumn.getColumnSize() + ")";
+		case COLUMN_TYPE_TIMESTAMP:
+			return "DATETIME YEAR to FRACTION";
+		case COLUMN_TYPE_LONGVARBINARY:
+			return "BYTE";
+		case COLUMN_TYPE_INTEGER:
+			return "INT";
+		case COLUMN_TYPE_DECIMAL:
+			return "DECIMAL(" + databaseColumn.getColumnSize() + "," + databaseColumn.getDecimalDigits() + ")";
+		case COLUMN_TYPE_DATE:
+			return "DATE";
+		case COLUMN_TYPE_DATETIME:
+			return "DATETIME YEAR to FRACTION";
+		case COLUMN_TYPE_BOOLEAN:
+			return "BOOLEAN";
+		default:
+			throw new IllegalArgumentException("no known column type");
+		}
+	}
+
+	private List<DatabaseColumn> getSortedColumns(final DatabaseTable databaseTable) {
+		final List<DatabaseColumn> columns = databaseTable.getColumns();
+		Collections.sort(columns, new Comparator<DatabaseColumn>() {
+
+			@Override
+			public int compare(final DatabaseColumn o1, final DatabaseColumn o2) {
+				return new Integer(o1.getOrdinalPosition()).compareTo(o2.getOrdinalPosition());
+			}
+		});
+		return columns;
 	}
 
 }
