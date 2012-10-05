6a2db35f30bd2ea4b265634ba41ea3be5621c797
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2012-10-05 09:25:37 +0800

    foreign key

---
+++ src/main/java/com/twistlet/soberspider/model/type/ForeignKey.java
@@ -0,0 +1,65 @@
+package com.twistlet.soberspider.model.type;
+
+public class ForeignKey {
+
+	private final String columnName;
+	private final String foreignColumn;
+	private final String foreignTable;
+
+	public ForeignKey(final String columnName, final String foreignColumn, final String foreignTable) {
+		super();
+		this.columnName = columnName;
+		this.foreignColumn = foreignColumn;
+		this.foreignTable = foreignTable;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public String getForeignColumn() {
+		return foreignColumn;
+	}
+
+	public String getForeignTable() {
+		return foreignTable;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((columnName == null) ? 0 : columnName.hashCode());
+		result = prime * result + ((foreignColumn == null) ? 0 : foreignColumn.hashCode());
+		result = prime * result + ((foreignTable == null) ? 0 : foreignTable.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final ForeignKey other = (ForeignKey) obj;
+		if (columnName == null) {
+			if (other.columnName != null)
+				return false;
+		} else if (!columnName.equals(other.columnName))
+			return false;
+		if (foreignColumn == null) {
+			if (other.foreignColumn != null)
+				return false;
+		} else if (!foreignColumn.equals(other.foreignColumn))
+			return false;
+		if (foreignTable == null) {
+			if (other.foreignTable != null)
+				return false;
+		} else if (!foreignTable.equals(other.foreignTable))
+			return false;
+		return true;
+	}
+
+}
