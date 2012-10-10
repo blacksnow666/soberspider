6947bb36939b6dab5825e87a463a252803b5a0ad
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2012-10-10 08:12:42 +0800

    simple caching mechanism so that listTableDependencies calls are minimized to once per table.

--- src/main/java/com/twistlet/soberspider/model/service/DatabaseServiceImpl.java
+++ src/main/java/com/twistlet/soberspider/model/service/DatabaseServiceImpl.java
@@ -1,7 +1,9 @@
 package com.twistlet.soberspider.model.service;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -27,13 +29,18 @@ public class DatabaseServiceImpl implements DatabaseService {
 
 	@Override
 	public List<String> sortTables(final DataSource dataSource, final List<String> tables) {
+		final Map<String, List<String>> map = new LinkedHashMap<>();
 		final List<String> listOk = new ArrayList<>();
 		final List<String> listToBeProcessed = new ArrayList<>(tables);
 		while (listToBeProcessed.size() > 0) {
 			final List<String> listAdded = new ArrayList<>();
 			for (final String table : listToBeProcessed) {
-				final List<String> deps = tableDependencyService.listTableDependencies(dataSource, table);
-				deps.remove(table);
+				List<String> deps = map.get(table);
+				if (deps == null) {
+					deps = tableDependencyService.listTableDependencies(dataSource, table);
+					deps.remove(table);
+					map.put(table, deps);
+				}
 				if (listOk.containsAll(deps)) {
 					listAdded.add(table);
 					listOk.add(table);
